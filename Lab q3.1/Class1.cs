6cb6c88926254f9eddd71b47154014b26d9ca485
krist <krist@LAPTOP-HIQLHLL6>
2022-11-25 00:14:57 +0500

    Добавьте файлы проекта.

---
+++ Lab q3.1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_13
+{
+    class Building
+    {
+        string adres;
+        int shirina;
+        int dlina;
+        int visota;
+
+        int Shirina
+        {
+            get
+            {
+                return shirina;
+            }
+            set
+            {
+                if (value < 0)
+                    shirina = 10;
+                else
+                    shirina = value;
+            }
+        }
+        int Dlina
+        {
+            get
+            {
+                return dlina;
+            }
+            set
+            {
+                if (value < 0)
+                    dlina = 10;
+                else
+                    dlina = value;
+            }
+           public Building(string adres, int shirina, int dlina, int visota)
+        {
+            this.adres = adres;
+            this.shirina = shirina;
+            this.dlina = dlina;
+            this.visota = visota;
+        }
+        public string Print()
+        {
+            return $"{adres}, {shirina}, {dlina}"
+        }
+    }
+}
+}
---
+++ Lab q3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_13
+{
+    class MultiBuilding : Building
+    {
+        string etagnost;
+        public MultiBuilding(string adres, int shirina, int dlina, int visota, string etagnost)
+        : base(adres, shirina, dlina, visota)
+        {
+            this.etagnost = etagnost;
+        }
+        public string Print()
+        {
+            string result = base.Print();
+            result += $"{etagnost}";
+            return result;
+        }
+    }
+
+}
